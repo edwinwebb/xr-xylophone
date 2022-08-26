6484d458c626969d786c777ab5f87cf4d59269d5
Edwin Webb <edwin.webb@gmail.com>
2022-08-26 11:29:58 -0700

    Add mallets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber'
 import { Debug, Physics } from '@react-three/rapier';
-import Model  from "./Xylophone";
+import Xylophone from "./Xylophone";
+import Mallet from './Mallet'
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <ambientLight intensity={0.5} />
       <Physics>
         <Debug />
-        <Model />
+        <Xylophone />
+        <Mallet position={[1,0,0]} />
+        <Mallet position={[-1,0,0]} />
       </Physics>
       <OrbitControls />
     </Canvas>
