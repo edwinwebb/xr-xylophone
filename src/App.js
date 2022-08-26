4aba97c286ea72ba67019636bb24d70c57a98e8e
Edwin Webb <edwin.webb@gmail.com>
2022-08-26 11:14:28 -0700

    Add phys

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber'
-import { Model }  from "./xylophone";
+import { Debug, Physics } from '@react-three/rapier';
+import Model  from "./Xylophone";
 
 function App() {
   return (
     <Canvas>
       <ambientLight intensity={0.5} />
-      <Model />
+      <Physics>
+        <Debug />
+        <Model />
+      </Physics>
       <OrbitControls />
     </Canvas>
   );
